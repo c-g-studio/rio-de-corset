085bcb32a79ba0a09ff030f313633b9b35b21b61
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-02-06 23:55:27 +0200

    fixed name

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: FC = ({ ...props }) => {
     <footer className="py-4" {...props}>
       <div className="container">
         <p className="text-center text-sm text-gray-600">
-          &copy; {new Date().getFullYear()} - Nex sta
+          &copy; {new Date().getFullYear()} - Next
         </p>
       </div>
     </footer>
