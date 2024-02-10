b0eae125149b9c4e36ea4752c6ee0fb65189b5e8
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-02-10 21:43:18 +0200

    qwe

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -13,36 +13,35 @@ interface NextPageProps {
 
 const Page: NextPage<NextPageProps> = async ({ params: { locale } }) => {
   // const { t, resources } = await initTranslations(locale, i18nNamespaces);
-//   const data = {
-//     "data": {
-//         "name": "test user",
-//         "number": "+3800000000",
-//         "email": "test@gmail.com",
-//         "city": "New York",
-//         "delivery_method": "Nova Poshta",
-//         "department_number": "1",
-//         "order_info": [
-//             {
-//                 "name": "corset",
-//                 "size": "XS",
-//                 "price": "3000 usd"
-//             },
-//              {
-//                 "name": "corset 1",
-//                 "size": "S",
-//                 "price": "300 грн"
-//             },
-//                     {
-//                 "name": "corset 3",
-//                 "size": "M",
-//                 "price": "100 грн"
-//             }
-//         ]
+  //   const data = {
+  //     "data": {
+  //         "name": "test user",
+  //         "number": "+3800000000",
+  //         "email": "test@gmail.com",
+  //         "city": "New York",
+  //         "delivery_method": "Nova Poshta",
+  //         "department_number": "1",
+  //         "order_info": [
+  //             {
+  //                 "name": "corset",
+  //                 "size": "XS",
+  //                 "price": "3000 usd"
+  //             },
+  //              {
+  //                 "name": "corset 1",
+  //                 "size": "S",
+  //                 "price": "300 грн"
+  //             },
+  //                     {
+  //                 "name": "corset 3",
+  //                 "size": "M",
+  //                 "price": "100 грн"
+  //             }
+  //         ]
 
-
-//     }
-// }
-//  await ordersAPI.addUkraineOrder(data);
+  //     }
+  // }
+  //  await ordersAPI.addUkraineOrder(data);
   // console.log(response);
   const { resources } = await initTranslations(locale, i18nNamespaces);
 
@@ -52,7 +51,6 @@ const Page: NextPage<NextPageProps> = async ({ params: { locale } }) => {
       locale={locale}
       resources={resources}
     >
-
       {/* 
       <section className="pb-40 pt-40">
 
