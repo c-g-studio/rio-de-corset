a0e77bb639eb0ddc717be457ded9f9a94509d0e2
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-02-16 15:52:33 +0200

    fatality

--- components/pages/home/CorsetsAnimate/CorsetsAnimate.tsx
+++ components/pages/home/CorsetsAnimate/CorsetsAnimate.tsx
@@ -4,9 +4,9 @@ import { FC } from 'react';
 export const CorsetsAnimate: FC = () => {
   return (
     <section className="fixed z-[-1] mt-[108px] w-[100%] pb-20 md:mt-[126px] md:pb-12 lg:mt-[200px] lg:pb-[202px]">
-      <div className="container flex h-[420px] sm:flex-row md:h-auto md:flex-col">
-        <div className="flex flex-col justify-between gap-5 overflow-hidden md:flex-row">
-          <ul className="flex shrink-0 animate-[colAnim_30s_linear_infinite] flex-col justify-between md:animate-[rowAnim_30s_linear_infinite] md:flex-row">
+      <div className="container flex h-[420px] flex-col md:h-auto">
+        <div className="flex flex-row justify-between gap-5 overflow-hidden">
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite] flex-row justify-between">
             <li className="shrink-0">
               <Image
                 src="/image/corsetsAnimate/top/01.png"
@@ -62,7 +62,7 @@ export const CorsetsAnimate: FC = () => {
               />
             </li>
           </ul>
-          <ul className="flex shrink-0 animate-[colAnim_30s_linear_infinite] flex-col justify-between md:animate-[rowAnim_30s_linear_infinite] md:flex-row">
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite] flex-row justify-between">
             <li className="shrink-0">
               <Image
                 src="/image/corsetsAnimate/top/01.png"
@@ -119,8 +119,8 @@ export const CorsetsAnimate: FC = () => {
             </li>
           </ul>
         </div>
-        <div className="flex flex-col justify-between gap-5 overflow-hidden md:flex-row">
-          <ul className="flex shrink-0 animate-[colAnim_30s_linear_infinite_reverse] flex-col justify-between md:animate-[rowAnim_30s_linear_infinite_reverse] md:flex-row">
+        <div className="flex flex-row justify-between gap-5 overflow-hidden">
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite_reverse] flex-row justify-between">
             <li className="shrink-0">
               <Image
                 src="/image/corsetsAnimate/bottom/01.png"
@@ -176,7 +176,7 @@ export const CorsetsAnimate: FC = () => {
               />
             </li>
           </ul>
-          <ul className="flex shrink-0 animate-[colAnim_30s_linear_infinite_reverse] flex-col justify-between md:animate-[rowAnim_30s_linear_infinite_reverse] md:flex-row">
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite_reverse] flex-row justify-between">
             <li className="shrink-0">
               <Image
                 src="/image/corsetsAnimate/bottom/01.png"
@@ -225,6 +225,120 @@ export const CorsetsAnimate: FC = () => {
             <li className="shrink-0">
               <Image
                 src="/image/corsetsAnimate/bottom/06.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-row justify-between gap-5 overflow-hidden md:hidden">
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite] flex-row justify-between">
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/01.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/02.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/03.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/04.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/05.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/06.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+          </ul>
+          <ul className="flex shrink-0 animate-[rowAnim_30s_linear_infinite] flex-row justify-between">
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/01.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/02.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/03.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/04.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/05.png"
+                width={208}
+                height={183}
+                alt="corset"
+                className="sm:w-[160px] md:w-[208px]"
+              />
+            </li>
+            <li className="shrink-0">
+              <Image
+                src="/image/corsetsAnimate/top/06.png"
                 width={208}
                 height={183}
                 alt="corset"
---
+++ components/pages/home/Shirts/ShirtsList.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { ProductCard } from '@/components/common/ProductCard/ProductCard';
+import { productsAPI } from '@/services/productsAPI';
+import { corsetsDto } from '@/types/corsetsDto';
+import { corsetAttributes } from '@/types/сorsetAttributes';
+import { AxiosResponse } from 'axios';
+import { FC, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useMedia } from 'react-use';
+interface ShirtsProps {
+  locale: string;
+}
+const IS_TABLET = '(min-width: 768px)';
+const IS_DESCTOP = '(min-width: 1440px)';
+
+export const ShirtsList: FC<ShirtsProps> = ({ locale }) => {
+  const { t } = useTranslation();
+  const [dataShirts, setDataShirts] =
+    useState<AxiosResponse<corsetsDto> | null>(null);
+  const isTablet = useMedia(IS_TABLET, false);
+  const isDesctop = useMedia(IS_DESCTOP, false);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await productsAPI.getCorsets(
+        locale,
+        isTablet && !isDesctop ? 2 : 3,
+      );
+      setDataShirts(data);
+    };
+    getData();
+  }, [t, locale, isTablet, isDesctop]);
+  return (
+    <ul className="mb-6 flex flex-col gap-4 md:flex-row md:gap-8 lg:gap-[68px]">
+      {dataShirts &&
+        dataShirts.data.data.map(
+          ({
+            id,
+            attributes,
+          }: {
+            id: number;
+            attributes: corsetAttributes;
+          }) => (
+            <ProductCard
+              key={id}
+              id={id}
+              attributes={attributes}
+              locale={locale}
+              category={'shirts'}
+            />
+          ),
+        )}
+    </ul>
+  );
+};
